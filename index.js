073b142b66095b66688c1562cff5218293e41112
Vipin <vipinrao03@gmail.com>
2021-10-22 22:48:16 +0530

    website upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const app = express();
+
+const port  = 7000;
+
+const templates = path.join(__dirname,"public");
+app.set("view engine","hbs");
+app.set("views",templates);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/",(req,res) =>{
+    res.render("index");
+});
+app.get("/register",(req,res) =>{
+    res.render("register");
+});
+app.use((req, res, next) => {
+    res.status(404).send(
+        "<h1>Page not found on the server</h1>"
+        );
+})
+app.listen(port,()=>{
+    console.log("server ready!!!");
+})
